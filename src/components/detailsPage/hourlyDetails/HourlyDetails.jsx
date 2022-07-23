0ad4c9ac951507f892e00d999162c05c9dec024c
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2022-07-23 15:27:49 +0530

    details pages's basic structure created

---
+++ src/components/detailsPage/hourlyDetails/HourlyDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HourlyDetails = ()=>{
+    return <div>Hourly Details</div>
+}
+
+export default HourlyDetails;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import DailyDetails from '../detailsPage/dailyDetails/DailyDetails'
+import HourlyDetails from '../detailsPage/hourlyDetails/HourlyDetails'
+import SunsetSunrise from '../detailsPage/sunsetSunrise/SunsetSunrise'
 import Input from '../inputbox/Input'
 import "./Home.css"
 function Home() {
   return (
-    <div className='body'><Input/></div>
+    <div className='body'>
+        <Input/>
+        <DailyDetails/>
+        <HourlyDetails/>
+        <SunsetSunrise/>
+        </div>
   )
 }
 
